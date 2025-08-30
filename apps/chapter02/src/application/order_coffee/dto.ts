eb3857a2a43cc075974312df264df5b4b4036b3c
maxubrq <hungtp.play@gmail.com>
2025-08-30 19:02:05 +0700

    feat: chapter 2 - coffee order

--- apps/chapter02/src/application/order_coffee/dto.ts
+++ apps/chapter02/src/application/order_coffee/dto.ts
@@ -3,10 +3,10 @@ import { BaseDrink, Currency, Size, Topping } from '../../domain/coffee_order';
 export type MoneyDTO = { amount: number; currency: Currency };
 
 export type CoffeeOrderDTO = {
-    id: string;
-    base: BaseDrink;
-    size: Size;
-    toppings: Topping[];
-    currency: Currency;
-    total?: MoneyDTO; // có thể embed khi trả response
+  id: string;
+  base: BaseDrink;
+  size: Size;
+  toppings: Topping[];
+  currency: Currency;
+  total?: MoneyDTO; // có thể embed khi trả response
 };
